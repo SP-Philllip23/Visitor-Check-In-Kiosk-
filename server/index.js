c76c9bfb51bc0f9482dd0c1064c25f2828fd7b30
Sary Phillip <garethphillip21@gmail.com>
2026-01-01 21:36:16 +0700

    yes now I have server index and now it working

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/health", (req, res) => {
+  res.json({ ok: true, message: "Visitor Check-In API running" });
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`API running at http://localhost:${PORT}`);
+});
